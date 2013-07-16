0bddd437516a95fa6e8347cf650029d35ef46990
Jeremy Bass <jeremybass26@gmail.com>
2013-07-16 08:18:14 -0700

    update
    Adds
    • preview (bool)
    • strip (string)
    
    **other changes**
    -- only pull css link elements not the favicon links and what not like
    that.  IE: not just any <link> but link[type='text/css'].
    -- added a iframe based preview that overlays and fills window making it
    look as if you have moved pages.

--- jPrint.js
+++ jPrint.js
@@ -19,7 +19,7 @@
 *
 */
 
-(function($) {
+(function($) {	
 	$.jprint = $.fn.jprint = function() {	
 		// Print a given set of elements
 		var isNode = function(o) {
@@ -55,42 +55,60 @@
 			useWindow : false,
 			skip : ".noprint",
 			iframe : true,
+			preview : true,
 			append : null,
-			prepend : null
+			prepend : null,
+			strip : null
 		};
 		options = $.extend(defaults, options);
 		if (options.useWindow && $this[0] === window) {
 			$this = $(document);
 		}
 		//Set styles if any to find
-		var styles = $("");
-		if (options.pageStyles) styles = $("style, link");
+		var css = $("");
+		if (options.pageStyles) css = $("style, link[type='text/css']");
 		if (!options.mediaPrint) $("link[media=print]").remove();
-
 		if (options.stylesheets) {
-			var sheets = options.stylesheets.split(',');
-			$.each(sheets,function(){
-				styles = $.merge(styles, $('<link rel="stylesheet" href="'+ sheets + '">'));
+			var sheetscvs = options.stylesheets.split(',');
+			$.each(sheetscvs,function(i,sheet){
+				css = $.merge(css, $('<link rel="stylesheet" type="text/css" href="'+ sheet + '">'));
 			});
 		}
+		var style = $("<div>").prepend(css.clone()).html();
 
+
+		if(options.strip!=null)$this.html($this.html().replace(options.strip, "" ));
 		var copy = $this.clone();
+		
 		copy = $("<span/>").append(copy);
 		copy.find(options.skip).remove();
-		copy.append(styles.clone());
+		//copy.prepend(styles.clone());
+		
 		copy.append($(options.append).clone());
 		copy.prepend($(options.prepend).clone());
 		var content = copy.html();
 		copy.remove();
 		
 		function newWindow(content){
-				w = window.open();
-				w.document.write(content);
-				w.document.close();
-				w.print();
-				w.close();	
+			w = window.open();
+			w.document.write(content);
+			w.document.close();
+			w.print();
+			w.close();
 		}
-		
+		function printHtml(w,options,strFrameName){
+			w.focus();
+			w.print();
+			window.setTimeout(function() {w.close();}, 3000);
+			if ($("#"+strFrameName).length) {
+				if(options.preview){
+					$("#"+strFrameName).css({ width:"0px",height:"0px",position:"absolute","z-index":999,left:"-9999em",top:"-9999em"});
+					$("html,body").css({overflow:"visiable"});
+					$("#printingControl").remove();
+				}
+				setTimeout(function(){$("#"+strFrameName).remove();},(60 * 1000));
+			}
+		}
 		
 		var w, wdoc;
 		if (options.iframe) {
@@ -99,20 +117,36 @@
 				// Create a new iFrame if none is given
 				var strFrameName = ("printer-" + (new Date()).getTime());
 				// Create an iFrame with the new name.
-				$iframe = $( "<iframe name='" + strFrameName + "' id='" + strFrameName + "' src='/'>" );
-				$iframe.css({ width:"0px",height:"0xp",position:"absolute","z-index":999,left:"-9999em",top:"-9999em"}).appendTo( $( "body:first" ) );
+				$iframe = $( "<iframe name='" + strFrameName + "' id='" + strFrameName + "' src='/' border='0'>" );
+				$iframe.css({ width:"0px",height:"0px",position:"absolute","z-index":999,left:"-9999em",top:"-9999em",border:"none"}).appendTo( $( "body:first" ) );
 
 				w = window.frames[ strFrameName ];
 				wdoc = w.document;
-				if(content.indexOf('<body')<0)content = '<!DOCTYPE html><html lang="en" dir="ltr"><head></head><body>'+content+'</body></html>';
+				if(content.indexOf('<body')<0){
+					content = '<!DOCTYPE html><html lang="en" dir="ltr"><head>'+style+'</head><body>'+content+'</body></html>';
+				}else{
+					$(content).find("head").append(style);
+				}
 				wdoc.write(content);
 				wdoc.close();
 				var $iframe = $("#"+strFrameName).contents();
-				w.focus();
-				w.print();
-				window.setTimeout(function() {w.close();}, 3000);
-				if ($iframe.length) {
-					setTimeout(function(){$iframe.remove();},(60 * 1000));
+				if(options.preview){
+					$("body").prepend('<div id="printingControl" style="background-color: gray; padding-top: 3px; padding-bottom: 3px; padding-left: 5px;height:24px;"><button id="printButton" style="margin-left: 20px;">Print</button><button id="cancelButton" style="margin-left: 20px;">Cancel</button></div>');
+					
+					$("#"+strFrameName).css({ width:"100%",height:$(window).height()-30,position:"fixed","z-index":9999,left:"0px",top:"30px","background-color":"#fff"});
+					$("html,body").css({overflow:"hidden"});
+
+					$("#printButton").click(function(){
+						printHtml(w,options,strFrameName);	
+					});
+					$("#cancelButton").click(function(){
+					   $("#"+strFrameName).remove();
+					   $("#printingControl").remove();
+					   $("html,body").css({overflow:"visiable"});
+					   return false;
+					});
+				}else{
+					printHtml(w,options,strFrameName);	
 				}
 			} catch (e) {
 				alert(e.message);
